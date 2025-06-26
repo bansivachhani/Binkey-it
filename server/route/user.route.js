903540f30850917bb7efb44ecabd67218200540c
Bansi <bansivachhani153@gmail.com>
2025-06-26 15:09:47 +0530

    added update user

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -4,7 +4,7 @@ import {
   verifyEmailController,
   loginController,
   logoutController,
-  uploadAvatar
+  uploadAvatar,updateUserDetails
 } from "../controllers/user.controller.js";
 import auth from "../middleware/auth.js";
 import upload from "../middleware/multer.js";
@@ -16,6 +16,7 @@ userRouter.post("/verify-email", verifyEmailController);
 userRouter.post('/login',loginController)
 userRouter.get("/logout",auth,logoutController)
 userRouter.put('/upload-avatar',auth,upload.single('avatar'),uploadAvatar)
+userRouter.put("/update-user",auth,updateUserDetails)
 
 
 export default userRouter;
