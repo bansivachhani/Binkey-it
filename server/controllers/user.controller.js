fa00657c0697269ded1ad1c6f182a6e4cdb4ff41
Bansi <bansivachhani153@gmail.com>
2025-06-26 13:12:27 +0530

    added login and register controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,143 @@
+import sendEmail from "../config/sendEmail.js";
+import UserModel from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
+
+export async function registerUserController(req, res) {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "provide email, name and password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      return res.json({
+        message: "Already register email",
+        error: true,
+        success: false,
+      });
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashPassword = await bcryptjs.hash(password, salt);
+
+    const payload = {
+      name,
+      email,
+      password: hashPassword,
+    };
+
+    const newUser = new UserModel(payload);
+    const save = await newUser.save();
+
+    const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code=${save._id}`;
+
+    const verifyEmail = await sendEmail({
+      sendTo: email,
+      subject: "Verifyy email from binkeyit",
+      html: verifyEmailTemplate({
+        name,
+        url: verifyEmailUrl,
+      }),
+    });
+
+    return res.json({
+      message: "User register successfully",
+      error: false,
+      success: true,
+      data: save,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function verifyEmailController(req, res) {
+  try {
+    const { code } = req.body;
+
+    const user = await UserModel.findOne({ _id: code });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid code",
+        error: true,
+        success: false,
+      });
+    }
+
+    const updateUser = await UserModel.updateOne(
+      { _id: code },
+      {
+        verify_email: true,
+      }
+    );
+
+    return res.json({
+      message: "Verify Email successfully",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+//login controller
+export async function loginController(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not registered",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (user.status !== "Active") {
+      return res.status(400).json({
+        message: "User not active ,Contact to Admin",
+        error: true,
+        success: false,
+      });
+    }
+
+    const checkPassword = await bcryptjs.compare(password, user.password);
+
+    if (!checkPassword) {
+      return res.status(400).json({
+        message: "Check your password",
+        error: true,
+        success: false,
+      });
+    }
+
+    
+
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import helmet from "helmet";
 import connectDB from "./config/connectDB.js";
+import userRouter from "./route/user.route.js";
 
 const app = express();
 
@@ -32,6 +33,8 @@ app.get("/", (req, res) => {
     message: "Server is running " + PORT,
   });
 });
+
+app.use("/api/user", userRouter);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
