55562aa13d9b42a9a2d4700bfc57dddc7e259849
Bansi <bansivachhani153@gmail.com>
2025-06-27 10:15:09 +0530

    aded register page modules

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,118 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa6";
 
 const Register = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+
+  const valideValue = Object.values(data).every(el=>el)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <section className=''>
+    <section className=" w-full container mx-auto px-2">
+      <div className="bg-white my-4 w-full max-w-lg mx-auto rounded p-9">
+        <p>Welcome to Binkeyit</p>
 
+        <form className="grid gap-6 mt-7 " onSubmit={handleSubmit}>
+          <div className="grid gap-1">
+            <label htmlFor="name">Name : </label>
+            <input
+              type="text"
+              id="name"
+              autoFocus
+              className="bg-blue-50 p-2 border rounded outline-none focus:border-primary-200"
+              name="name"
+              value={data.name}
+              onChange={handleChange}
+              placeholder="Enter your name"
+            />
+          </div>
+
+          <div className="grid gap-1">
+            <label htmlFor="email">Email : </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-blue-50 p-2 border rounded outline-none focus:border-primary-200"
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div className="grid gap-1">
+            <label htmlFor="password">Password : </label>
+            <div className="bg-blue-50 p-2 border rounded flex items-center focus-within:border-primary-200">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                className="w-full outline-none"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+                placeholder="Enter your password"
+              />
+              <div
+                onClick={() => setShowPassword((preve) => !preve)}
+                className="cursor-pointer"
+              >
+                {showPassword ? <FaRegEye /> : <FaEyeSlash />}
+              </div>
+            </div>
+          </div>
+
+          <div className="grid gap-1">
+            <label htmlFor="confirmPassword">Confirm Password : </label>
+            <div className="bg-blue-50 p-2 border rounded flex items-center focus-within:border-primary-200">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                className="w-full outline-none"
+                name="confirmPassword"
+                value={data.showConfirmPassword}
+                onChange={handleChange}
+                placeholder="Enter your confirm password"
+              />
+              <div
+                onClick={() => setShowConfirmPassword((preve) => !preve)}
+                className="cursor-pointer"
+              >
+                {showConfirmPassword ? <FaRegEye /> : <FaEyeSlash />}
+              </div>
+            </div>
+          </div>
+
+          <button
+            className={` ${valideValue ? "bg-green-800 hover:bg-green-700" : " bg-gray-500" } text-white py-2 rounded font-semibold my-3 tracking-wide`}
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
