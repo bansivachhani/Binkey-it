1100b3bf0e08da16b7cb94ad3e4e885d47ed800f
Bansi <bansivachhani153@gmail.com>
2025-06-27 11:18:04 +0530

    added a forgot password page

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,3 +1,5 @@
+import OtpVerification from "../pages/OtpVerification";
+
 export const baseURL = "http://localhost:8080";
 
 const SummaryApi = {
@@ -5,10 +7,18 @@ const SummaryApi = {
     url: "/api/user/register",
     method: "post",
   },
-  login : {
+  login: {
     url: "/api/user/login",
     method: "post",
-  }
+  },
+  forgot_password: {
+    url: "/api/user/forgot-password",
+    method: "put",
+  },
+  forgot_password_otp_verification: {
+    url: "/api/user/verify-forgot-password-otp",
+    method: "put",
+  },
 };
 
 export default SummaryApi;
--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -1,34 +1,43 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App'
-import Home from '../pages/Home'
-import SearchPage from '../pages/SearchPage';
-import Register from '../pages/Register';
-import Login from '../pages/Login';
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import SearchPage from "../pages/SearchPage";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import ForgotPassword from "../pages/ForgotPassword";
+import OtpVerification from "../pages/OtpVerification";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        children : [
-            {
-                path: "",
-                element:<Home/>
-            },
-            {
-                path : "search",
-                element : <SearchPage/>
-            },
-            {
-                path : "login",
-                element : <Login/>
-            },
-            {
-                path : "register",
-                element : <Register/>
-            }
-        ]
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "forgot-password",
+        element: <ForgotPassword />,
+      },
+      {
+        path : "verification-otp",
+        element : <OtpVerification/>
+      }
+    ],
+  },
+]);
 
-    }
-])
-
-export default router
+export default router;
