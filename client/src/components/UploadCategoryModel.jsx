cff13f293e6eb274ca47a222b9692e8b9dbfaeea
Bansi <bansivachhani153@gmail.com>
2025-06-28 09:59:14 +0530

    design category page

--- client/src/components/UploadCategoryModel.jsx
+++ client/src/components/UploadCategoryModel.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoClose } from 'react-icons/io5'
 
-const UploadCategoryModel = () => {
+const UploadCategoryModel = ({close}) => {
+
+    const [data,setData] = useState({
+        name : "",
+        image : ""
+    })
+
+    const handleOnChange = (e)=>{
+        const {name,value} = e.target
+
+        setData((preve)=>{
+            return{
+                ...preve,
+                [name] : value
+            }
+        })
+    }
+
+   
+
   return (
-    <div>UploadCategoryModel</div>
+    <section className='fixed top-0 bottom-0 left-0 right-0 p-4 bg-neutral-800 bg-opacity-60 flex items-center justify-center'>
+        <div className='bg-white max-w-4xl w-full p-4 rounded'>
+              <div className='flex items-center justify-between'>
+                <h1 className=' font-semibold'>Category</h1>
+                 <button onClick={close} className='w-fit block ml-auto'>
+                <IoClose size={25}/>
+                </button>
+              </div>
+
+              <form action="" className='my-3 grid gap-2'>
+                <div className='grid gap-1'>
+                    <label id='categoryName'>Name</label>
+                    <input
+                        type='text'
+                        id='categoryName'
+                        placeholder='Enter category name'
+                        value={data.name}
+                        name='name'
+                        onChange={handleOnChange}
+                        className='bg-blue-50 p-2 border border-blue-100 focus-within:border-primary-200 outline-none rounded'
+                    />
+                </div>
+                <div className='grid gap-1'>
+                    <p>Image</p>
+                    <div className='flex gap-4 flex-col lg:flex-row items-center'>
+                        <div className='border bg-blue-50 h-36 w-full lg:w-36  flex items-center justify-center rounded'>
+                        <p className='text-sm text-neutral-400'>No Image</p>
+                    </div>
+                    <button disabled={!data.name} className={`
+                        ${!data.name ? "bg-gray-400" : "bg-primary-200"}
+                        px-4 py-2 rounded
+                        `}>Upload Image</button>
+                    </div>
+                </div>
+              </form>
+        </div>
+    </section>
   )
 }
 
--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UploadCategoryModel from '../components/UploadCategoryModel'
 
 const CategoryPage = () => {
+
+    const [openUploadCategory,setOpenUploadCategory] = useState(false)
   return (
     <section>
         <div className='p-2  bg-white shadow-md flex items-center justify-between '>
             <h2 className='font-semibold'> Category </h2>
-            <button className='text-sm border border-primary-200 hover:bg-primary-200 px-3 py-1 rounded'>Add Category</button>
+            <button onClick={()=>setOpenUploadCategory(true)} className='text-sm border border-primary-200 hover:bg-primary-200 px-3 py-1 rounded'>Add Category</button>
         </div>
+
+        {
+            openUploadCategory && (
+                <UploadCategoryModel close={()=>setOpenUploadCategory(false)}/>
+            )
+        }
+
     </section>
   )
 }
