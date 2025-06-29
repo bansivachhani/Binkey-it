1376a209ce0738de3caa23bafb8681644e9cdfdf
Bansi <bansivachhani153@gmail.com>
2025-06-29 18:16:34 +0530

    added a home ui

--- client/src/components/CardProduct.jsx
+++ client/src/components/CardProduct.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { DisplayPriceInRupees } from '../utils/DisplayPriceInRupees'
 
 const CardProduct = ({data}) => {
   return (
-    <div>CardProduct</div>
+    <div className='border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded cursor-pointer bg-white '>
+      <div className='min-h-20 w-full max-h-24 lg:max-h-32 rounded overflow-hidden'>
+        <img 
+                src={data.image[0]}
+                className='w-full h-full object-scale-down lg:scale-125'
+            />
+      </div>
+
+      <div className='rounded text-xs w-fit p-[1px] px-2 text-green-600 bg-green-100'>
+        10 min
+      </div>
+
+      <div className='px-2 lg:px-0 font-medium text-ellipsis text-sm lg:text-base line-clamp-2'>
+        {data.name}
+      </div>
+
+      <div className='w-fit gap-1 px-2 lg:px-0 text-sm lg:text-base'>
+        {data.unit}
+      </div>
+
+    <div className='px-2 lg:px-0 flex items-center justify-between gap-1 lg:gap-3 text-sm lg:text-base'>
+      <div className='flex items-center gap-1'>
+        <div className='font-semibold'>
+            {DisplayPriceInRupees(data.price)}
+        </div>
+      </div>
+          
+        <div className=''>
+            <button className='bg-green-600 hover:bg-green-700 text-white px-4 py-1 rounded'>
+                Add
+            </button>
+        </div>
+    </div>
+
+    </div>
   )
 }
 
--- client/src/components/CategoryWiseProductDisplay.jsx
+++ client/src/components/CategoryWiseProductDisplay.jsx
@@ -5,6 +5,7 @@ import Axios from '../utils/Axios'
 import AxiosToastError from '../utils/AxiosToastError'
 import CardLoading from './CardLoading'
 import CardProduct from './CardProduct'
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
 
 const CategoryWiseProductDisplay = ({id,name}) => {
@@ -12,6 +13,8 @@ const CategoryWiseProductDisplay = ({id,name}) => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
     const loadingCardNumber = new Array(6).fill(null)
+    const containerRef = useRef()
+
 
      const fetchCategoryWiseProduct = async () => {
         try {
@@ -40,6 +43,14 @@ const CategoryWiseProductDisplay = ({id,name}) => {
         fetchCategoryWiseProduct()
     }, [])
 
+       const handleScrollRight = () => {
+        containerRef.current.scrollLeft += 200
+    }
+
+    const handleScrollLeft = () => {
+        containerRef.current.scrollLeft -= 200
+    }
+
   return (
     <div>
         <div className='container mx-auto p-4 flex items-center justify-between gap-4'>
@@ -47,7 +58,8 @@ const CategoryWiseProductDisplay = ({id,name}) => {
                 <Link  className='text-green-600 hover:text-green-400'>See All</Link>
             </div>
             <div className='relative flex items-center '>
-                <div className=' flex gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-x-scroll scrollbar-none scroll-smooth'>
+                <div  ref={containerRef}
+                 className=' flex gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-x-scroll scrollbar-none scroll-smooth' >
 
                 {
                     loading &&
@@ -66,9 +78,19 @@ const CategoryWiseProductDisplay = ({id,name}) => {
                                 />
                             )
                         })
-                }
+                    }                  
 
                 </div>
+                 <div className='w-full left-0 right-0 container mx-auto  px-2  absolute hidden lg:flex justify-between'>
+                    <button onClick={handleScrollLeft} className='z-10 relative bg-white hover:bg-gray-100 shadow-lg text-lg p-2 rounded-full'>
+                        <FaAngleLeft />
+                    </button>
+                    <button onClick={handleScrollRight} className='z-10 relative  bg-white hover:bg-gray-100 shadow-lg p-2 text-lg rounded-full'>
+                        <FaAngleRight />
+                    </button>
+                </div>
+
+
             </div>
     </div>
   )
