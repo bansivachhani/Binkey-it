f09c2b9973bb48c8c92cb2397e7f4a82774decc8
Bansi <bansivachhani153@gmail.com>
2025-06-29 10:52:59 +0530

    added a subcategory module complete

--- client/src/pages/SubCategoryPage.jsx
+++ client/src/pages/SubCategoryPage.jsx
@@ -150,7 +150,7 @@ const SubCategoryPage = () => {
         </button>
       </div>
 
-      <div>
+      <div className="overflow-auto w-full max-w-[95vw]">
         <DisplayTable data={data} column={column} />
       </div>
 
