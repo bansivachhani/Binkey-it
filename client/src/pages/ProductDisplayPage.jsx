53afa5f530e989e33468d7a7d790515090e8f301
Bansi <bansivachhani153@gmail.com>
2025-07-01 15:05:28 +0530

    added a design

--- client/src/pages/ProductDisplayPage.jsx
+++ client/src/pages/ProductDisplayPage.jsx
@@ -11,6 +11,7 @@ const ProductDisplayPage = () => {
     name : "",
     image: [],
   })
+  const [image,setImage] = useState(0)
   const [loading, setLoading] = useState(false)
 
   const fetchProductDetails = async()=>{
@@ -26,6 +27,7 @@ const ProductDisplayPage = () => {
 
         if(responseData.success){
           setData(responseData.data)
+          //setImage(responseData.data.image[0])
         }
     } catch (error) {
       AxiosToastError(error)
@@ -38,9 +40,33 @@ const ProductDisplayPage = () => {
     fetchProductDetails()
   },[params])
 
-  console.log(data)
+  //console.log(data)
   return (
-    <div>ProductDisplayPage</div>
+    <section className='container mx-auto p-4 grid lg:grid-cols-2 '>
+      <div>
+            <div className='bg-white lg:min-h-[65vh] lg:max-h-[65vh] rounded min-h-56 max-h-56 h-full w-full'>
+              <img
+                src={data.image[image]}
+                className='w-full h-full object-scale-down'
+              />
+            </div>
+            <div className='flex items-center justify-center gap-3 my-2'>
+              {
+                data.image.map((img,index)=>{
+                  return(
+                    <div key={img+index+"point"} className={`bg-slate-200 w-3 h-3 lg:w-5 lg:h-5 rounded-full ${index === image && "bg-slate-300"}`}></div>
+                  )
+                })
+              }
+            </div>
+            
+
+
+      </div>
+      <div>
+
+      </div>
+    </section>
   )
 }
 
