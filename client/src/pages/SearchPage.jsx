a86cc605afb666e6cf896a4384c0b7aa03503ca8
Bansi <bansivachhani153@gmail.com>
2025-07-02 14:26:06 +0530

    added scrolling to search page

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -4,8 +4,9 @@ import { useSelector } from 'react-redux'
 import SummaryApi from '../common/SummaryApi'
 import Axios from '../utils/Axios'
 import AxiosToastError from '../utils/AxiosToastError'
-//import InfiniteScroll from 'react-infinite-scroll-component'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { useLocation } from 'react-router-dom'
+import CardProduct from '../components/CardProduct'
 
 const SearchPage = () => {
 
@@ -57,12 +58,31 @@ const SearchPage = () => {
 
   console.log("page",page)
 
+  const handleFetchMore = ()=>{
+    if(totalPage > page){
+      setPage(preve => preve + 1)
+    }
+  }
+
   return (
     <section  className='bg-white'>
       <div className='container mx-auto p-4'>
         <p className='font-semibold'>Search Results: {data.length}  </p>
      
+     <InfiniteScroll
+              dataLength={data.length}
+              hasMore={true}
+              next={handleFetchMore}
+        >
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 py-4 gap-4'>
+
+       {
+                data.map((p,index)=>{
+                  return(
+                    <CardProduct data={p} key={p?._id+"searchProduct"+index}/>
+                  )
+                })
+     }
 
       {/**loading data */}
       {
@@ -75,6 +95,7 @@ const SearchPage = () => {
               )
       }
       </div>
+      </InfiniteScroll>
 
 
        </div>
