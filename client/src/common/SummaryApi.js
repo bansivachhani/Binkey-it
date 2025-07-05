f90ae7e53c58e06825fdafe0fab31c9257b2b680
Bansi <bansivachhani153@gmail.com>
2025-07-05 11:42:27 +0530

    added a vercel file

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -5,7 +5,7 @@ import {
 } from "../../../server/controllers/product.controller";
 import { logout } from "../store/userSlice";
 
-export const baseURL = "http://localhost:8080";
+export const baseURL = import.meta.env.VITE_API_URL;
 
 const SummaryApi = {
   register: {
