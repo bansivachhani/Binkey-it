ec00d577760f23f2d6aff16bb6376aea683f782d
Bansi Vachhani <121160924+bansivachhani@users.noreply.github.com>
2025-07-08 15:20:39 +0530

    Delete Desktop/port/portfolio directory

--- Desktop/port/portfolio/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="text-3xl font-bold text-blue-600 p-10 text-center">
-      Full Stack Developer Portfolio
-    </div>
-  );
-}
-
-export default App;
